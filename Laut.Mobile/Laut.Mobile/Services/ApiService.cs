3ed13e25c80347094c5f4d9937c65878e84e421c
Jonathan Braga <jonathanb2br@gmail.com>
2017-09-19 22:06:04 -0300

    Api Comunication

---
+++ Laut.Mobile/Laut.Mobile/Services/ApiService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laut.Mobile.Models;
+using System.Xml.Linq;
+
+namespace Laut.Mobile.Services
+{
+    public class ApiService : IApiService
+    {
+        public async Task<List<News>> GetNews()
+        {
+            XDocument feedXML = XDocument.Load("https://jonathanbraga.github.io/feed.xml");
+
+            var news = new List<News>();
+            var feeds = from feed in feedXML.Descendants("entry")
+                        select new
+                        {
+                           Title = feed.Element("title").Value
+                        };
+            return news;
+        }
+    }
+}
---
+++ Laut.Mobile/Laut.Mobile/Services/IApiService.cs
@@ -0,0 +1,14 @@
+﻿using Laut.Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laut.Mobile.Services
+{
+    public interface IApiService
+    {
+        Task<List<News>> GetNews();
+    }
+}
